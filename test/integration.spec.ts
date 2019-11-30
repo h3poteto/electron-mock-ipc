41891fa38265617c845ae1cac41deff05978ac59
AkiraFukushima <h3.poteto@gmail.com>
2019-11-30 19:06:27 +0900

    refs #32 Fix existing tests

--- test/integration.spec.ts
+++ test/integration.spec.ts
@@ -1,7 +1,7 @@
 import { IpcRendererEvent, IpcMainEvent } from 'electron'
 import createIPCMock, { ipcMain, ipcRenderer } from '@/index'
 
-describe('send from main', () => {
+describe('send event from renderer to main', () => {
   let ipcMain: ipcMain
   let ipcRenderer: ipcRenderer
 
@@ -11,29 +11,31 @@ describe('send from main', () => {
     ipcRenderer = mocked.ipcRenderer
   })
 
-  it('should be received', () => {
-    const testMessage = 'test'
-    ipcMain.once('test-event', (ev: IpcMainEvent, obj: string) => {
-      expect(obj).toEqual(testMessage)
+  describe('send and once', () => {
+    it('should be received in main', () => {
+      const testMessage = 'test'
+      ipcMain.once('test-event', (_ev: IpcMainEvent, obj: string) => {
+        expect(obj).toEqual(testMessage)
+      })
+
+      ipcRenderer.send('test-event', testMessage)
     })
 
-    ipcRenderer.send('test-event', testMessage)
-  })
+    it('should be resent and received', () => {
+      const testMessage = 'test'
+      ipcRenderer.once('response-test-event', (_ev: IpcRendererEvent, obj: string) => {
+        expect(obj).toEqual(testMessage)
+      })
+      ipcMain.once('test-event', (ev: IpcMainEvent, obj: string) => {
+        ev.sender.send('response-test-event', obj)
+      })
 
-  it('should be resend and receive', () => {
-    const testMessage = 'test'
-    ipcRenderer.once('response-test-event', (ev: IpcRendererEvent, obj: string) => {
-      expect(obj).toEqual(testMessage)
+      ipcRenderer.send('test-event', testMessage)
     })
-    ipcMain.once('test-event', (ev: IpcMainEvent, obj: string) => {
-      ev.sender.send('response-test-event', obj)
-    })
-
-    ipcRenderer.send('test-event', testMessage)
   })
 })
 
-describe('send from renderer', () => {
+describe('send event from main to renderer', () => {
   let ipcMain: ipcMain
   let ipcRenderer: ipcRenderer
 
@@ -43,24 +45,26 @@ describe('send from renderer', () => {
     ipcRenderer = mocked.ipcRenderer
   })
 
-  it('should be received', () => {
-    const testMessage = 'test'
-    ipcRenderer.once('test-event', (ev: IpcRendererEvent, obj: string) => {
-      expect(obj).toEqual(testMessage)
+  describe('send and once', () => {
+    it('should be received in renderer', () => {
+      const testMessage = 'test'
+      ipcRenderer.once('test-event', (_ev: IpcRendererEvent, obj: string) => {
+        expect(obj).toEqual(testMessage)
+      })
+
+      ipcMain.send('test-event', testMessage)
     })
 
-    ipcMain.send('test-event', testMessage)
-  })
+    it('should be resent and received', () => {
+      const testMessage = 'test'
+      ipcMain.once('response-test-event', (_ev: IpcMainEvent, obj: string) => {
+        expect(obj).toEqual(testMessage)
+      })
+      ipcRenderer.once('test-event', (ev: IpcRendererEvent, obj: string) => {
+        ev.sender.send('response-test-event', obj)
+      })
 
-  it('should be resend and receive', () => {
-    const testMessage = 'test'
-    ipcMain.once('response-test-event', (ev: IpcMainEvent, obj: string) => {
-      expect(obj).toEqual(testMessage)
+      ipcMain.send('test-event', testMessage)
     })
-    ipcRenderer.once('test-event', (ev: IpcRendererEvent, obj: string) => {
-      ev.sender.send('response-test-event', obj)
-    })
-
-    ipcMain.send('test-event', testMessage)
   })
 })
